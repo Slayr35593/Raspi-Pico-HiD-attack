3796d38f97a0d25317fefde0e5014048df446a21
SLYR <peteraltforstuff@gmail.com>
2025-11-09 22:35:19 -0500

    Update SYSRIP.ps1
    Removed the launch of Notepad and exit prompt from the script.

--- Pi Pico HID attack/SYSRIP.ps1
+++ Pi Pico HID attack/SYSRIP.ps1
@@ -2,4 +2,3 @@ start-process devmgmt.msc
 ping 1.1.1.1
 Write-Host "The device is in sterile mode, no payload has been executed." -BackgroundColor Red
 Read-Host -Prompt "Press Enter to exit"
-Start-Process notepad.exe
